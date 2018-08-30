d6b8d82a66a8d6bff6b35e462201a66a03c217f2
Harshit Satyaseel <hsatyaseel@gmail.com>
2018-08-30 11:58:59 +0530

    Initial Commit

---
+++ MyGoals/Controller/GoalsVC.swift
@@ -0,0 +1,23 @@
+//
+//  GoalsVC.swift
+//  MyGoals
+//
+//  Created by Harshit Satyaseel on 30/08/18.
+//  Copyright © 2018 Harshit Satyaseel. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBOutlet weak var goalButtonWasPressed: UIButton!
+    
+}
---
+++ MyGoals/Views/GoalCell.swift
@@ -0,0 +1,30 @@
+//
+//  GoalCell.swift
+//  MyGoals
+//
+//  Created by Harshit Satyaseel on 30/08/18.
+//  Copyright © 2018 Harshit Satyaseel. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    
+    @IBOutlet weak var goalDescription: UILabel!
+    @IBOutlet weak var goalType: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    // function to pass proper data when we actually begin or fill data from our coreData model.
+    
+    func ConfigureCell(description: String, type: String, goalProgress: Int){
+        
+        self.goalDescription.text = description
+        self.goalType.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressLbl) // when we pass a int value in tableview do this way
+        
+        
+        
+    }
+    
+}
